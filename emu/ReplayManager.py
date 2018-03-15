8a88333062abe4c91eb57c147977426831502175
Yngve AAdlandsvik <ymgve.aa@gmail.com>
2018-03-15 20:53:48 +0100

    Changed to new legacy replay data format

--- emu/ReplayManager.py
+++ emu/ReplayManager.py
@@ -2,78 +2,63 @@ import cStringIO, logging, os, sqlite3, struct, random
 
 from emu.Util import *
 
-class ReplayHeader(object):
+class Replay(object):
     def __init__(self):
         pass
         
-    def create(self, sio):
-        ghostID = sio.read(4)
-        if len(ghostID) != 4:
-            return False
-            
-        self.ghostID = struct.unpack("<I", ghostID)[0]
-        
-        self.name = readcstring(sio)
-        self.data = sio.read(40)
-        
-        self.blockID, self.posx, self.posy, self.posz, self.angx, self.angy, self.angz, self.messageID, self.mainMsgID, self.addMsgCateID = struct.unpack("<iffffffIII", self.data)
-        
-        return True
-        
-    def to_bin(self):
-        res = struct.pack("<I", self.ghostID)
-        res += self.name + "\x00"
-        res += self.data
+    def unserialize(self, data):
+        sio = cStringIO.StringIO(data)
+        self.ghostID = struct.unpack("<I", sio.read(4))[0]
+        self.characterID = readcstring(sio)
+        self.blockID, self.posx, self.posy, self.posz, self.angx, self.angy, self.angz = struct.unpack("<iffffff", sio.read(28))
+        self.messageID, self.mainMsgID, self.addMsgCateID = struct.unpack("<iii", sio.read(12))
+        self.replayBinary = readcstring(sio)
+        assert sio.read() == ""
+        self.legacy = 1
+
+    def serialize_header(self):
+        res = ""
+        res += struct.pack("<I", self.ghostID)
+        res += self.characterID + "\x00"
+        res += struct.pack("<iffffff", self.blockID, self.posx, self.posy, self.posz, self.angx, self.angy, self.angz)
+        res += struct.pack("<iii", self.messageID, self.mainMsgID, self.addMsgCateID)
         return res
-        
+
 class ReplayManager(object):
     def __init__(self):
-        self.replayheaders = {}
-        self.replaydata = {}
+        self.replays = {}
         
-        f = open("replayheaders.bin", "rb")
+        f = open("data/legacyreplaydata.bin", "rb")
         while True:
-            header = ReplayHeader()
-            res = header.create(f)
-            if not res:
+            data = f.read(4)
+            if len(data) == 0:
                 break
-                
-            if header.blockID not in self.replayheaders:
-                self.replayheaders[header.blockID] = []
-                
-            if (header.messageID, header.mainMsgID, header.addMsgCateID) != (0, 0, 0):
-                self.replayheaders[header.blockID].append(header)
-                
-        logging.info("Finished reading replay headers")
-        
-        f = open("replaydata.bin", "rb")
-        while True:
-            ghostID = f.read(4)
-            if len(ghostID) != 4:
-                break
-            ghostID = struct.unpack("<I", ghostID)[0]
+            sz = struct.unpack("<I", data)[0]
             
-            data = readcstring(f)
-            
-            self.replaydata[ghostID] = data
+            rep = Replay()
+            rep.unserialize(f.read(sz))
+                    
+            self.replays[rep.ghostID] = rep
             
         logging.info("Finished reading replay data")
         
     def handle_getReplayList(self, params):
         blockID = make_signed(int(params["blockID"]))
         replayNum = int(params["replayNum"])
-        
+
         data = struct.pack("<I", replayNum)
-        for i in xrange(replayNum):
-            header = random.choice(self.replayheaders[blockID])
-            data += header.to_bin()
-            
+        while replayNum > 0:
+            rep = random.choice(self.replays.values())
+            if blockID == rep.blockID:
+                data += rep.serialize_header()
+                replayNum -= 1
+
         return 0x1f, data
         
     def handle_getReplayData(self, params):
         ghostID = int(params["ghostID"])
         
-        ghostdata = self.replaydata[ghostID]
-        data = struct.pack("<II", ghostID, len(ghostdata)) + ghostdata
+        replayBinary = self.replays[ghostID].replayBinary
+        data = struct.pack("<II", ghostID, len(replayBinary)) + replayBinary
             
         return 0x1e, data
